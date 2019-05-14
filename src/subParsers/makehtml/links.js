3277792c1219be674ac724a033d74452a97eacf4
Daniel Dyssegaard Kallick <ddyssegaardk@gmail.com>
2019-05-14 17:49:58 -0400

    make sure there is a space between a tag attributes

--- src/subParsers/makehtml/links.js
+++ src/subParsers/makehtml/links.js
@@ -132,7 +132,7 @@
     //evt = createEvent(rgx, evtRootName + '.captureEnd', wholeMatch, text, id, url, title, options, globals);
 
     var className = options.linkClassName || '';
-    var result = '<a' + 'class="' + className + '"' + 'href="' + url + '"' + title + target + '>' + text + '</a>';
+    var result = '<a ' + 'class="' + className + '" ' + 'href="' + url + '"' + title + target + '>' + text + '</a>';
 
     //evt = createEvent(rgx, evtRootName + '.beforeHash', wholeMatch, text, id, url, title, options, globals);
 
