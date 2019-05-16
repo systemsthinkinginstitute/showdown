849864f328c1d9d42402ba4ccfd4db43d2aa0099
David Chester <david@metamap.me>
2019-05-16 11:18:16 +0000

    don't leave html in generated markdown

---
+++ src/subParsers/makemarkdown/block.js
@@ -0,0 +1,18 @@
+showdown.subParser('makeMarkdown.block', function (node, globals) {
+  'use strict';
+
+  var txt = '';
+
+  if (node.hasChildNodes()) {
+    var children = node.childNodes,
+        childrenLength = children.length;
+    for (var i = 0; i < childrenLength; ++i) {
+      txt += showdown.subParser('makeMarkdown.node')(children[i], globals);
+    }
+  }
+
+  // some text normalization
+  txt = txt.trim();
+
+  return txt;
+});
